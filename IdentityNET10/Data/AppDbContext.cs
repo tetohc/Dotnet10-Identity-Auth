b633d1d04425cddd9484dec1192dc5a0d09cfc73
Jerry Hurtado Castillo <jerryalonso01@gmail.com>
2025-11-26 11:38:57 -0600

    Add project files.

---
+++ IdentityNET10/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityNET10.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
